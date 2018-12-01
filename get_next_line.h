5cda71db91862a1285a9132fe76b252dfdf7409e
lukefreeman42 <lukefreeman@protonmail.com>
2018-11-30 17:15:58 -0800

    final??

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 21:29:49 by llelias           #+#    #+#             */
-/*   Updated: 2018/11/30 14:38:03 by llelias          ###   ########.fr       */
+/*   Updated: 2018/11/30 17:15:18 by llelias          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 20
-# define MAX_FD 2048
+# define MAX_FD 256
 # include <libft.h>
 
 int	get_next_line(const int fd, char **line);
