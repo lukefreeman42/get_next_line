d79a1f43ed986e2cd6346b9c70aff1784a45e6f5
lukefreeman42 <lukefreeman@protonmail.com>
2018-12-06 10:20:22 -0800

    Macro fix

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 21:29:49 by llelias           #+#    #+#             */
-/*   Updated: 2018/12/06 09:55:44 by llelias          ###   ########.fr       */
+/*   Updated: 2018/12/06 10:20:08 by llelias          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 # define BUFF_SIZE 20
 # define MAX_FD 256
-# define VALID_FD(x) (x >= 0 && x < MAX_FD ? 1 : 0)
+# define VALID_FD(x) (x >= 0 && x < MAX_FD)
 # include <libft.h>
 
 int	get_next_line(const int fd, char **line);
