1c3aa5707fd2c41031af692aa536d9fed82d4cfd
lukefreeman42 <lukefreeman@protonmail.com>
2018-12-06 09:57:32 -0800

    less lines, handles final error check

--- get_next_line.c
+++ get_next_line.c
@@ -5,44 +5,33 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/04 10:23:55 by llelias           #+#    #+#             */
-/*   Updated: 2018/12/05 17:14:57 by llelias          ###   ########.fr       */
+/*   Created: 2018/12/06 09:25:42 by llelias           #+#    #+#             */
+/*   Updated: 2018/12/06 09:54:34 by llelias          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		read_to_b(int fd, char *b[])
+int		gnl(int fd, char *b[], char **line)
 {
 	int		st;
 	char	rb[BUFF_SIZE + 1];
-
-	while ((st = read(fd, rb, BUFF_SIZE)) > 0)
-	{
-		rb[st] = '\0';
-		b[fd] = ft_fstrjoin(b[fd], rb, 1);
-		if (ft_strchr(b[fd], '\n'))
-			break;
-	}
-	return (st);
-}
-
-int		grab_line(int fd, char *b[], char **line)
-{
 	char	*nl_p;
 	char	*tmp;
-	
+
+	while (!(nl_p = ft_strchr(b[fd], '\n')) &&
+			(st = read(fd, rb, BUFF_SIZE)) > 0 &&
+		   !(rb[st] = '\0'))
+		b[fd] = ft_fstrjoin(b[fd], rb, 1);
+	tmp = b[fd];
+	if (st == -1)
+		return (-1);
 	if (ft_strlen(b[fd]))
 	{
-		nl_p = ft_strchr(b[fd], '\n');
-		tmp = b[fd];
-		if (nl_p)
-		{
-			*nl_p = '\0';
+		if (nl_p && !(*nl_p = '\0'))
 			b[fd] = ft_strdup(++nl_p);
-		}
-		*line = ft_fstrdup(tmp);
-		if (ft_strlen(b[fd]))
+		*line = ft_strdup(tmp);
+		if (ft_strlen(b[fd]) || st)
 			return (1);
 	}
 	return (0);
@@ -50,10 +39,10 @@ int		grab_line(int fd, char *b[], char **line)
 
 int		get_next_line(int fd, char **line)
 {
-	static char		*b[MAX_FD];
-	if (fd >= 0 && !b[fd])
-		b[fd] = ft_strnew(0);	
-	if (fd >= 0 && read_to_b(fd, b) != -1)
-		return (grab_line(fd, b, line));
-	return (-1);
+	static char *b[MAX_FD];
+	if (!VALID_FD(fd))
+		return (-1);
+	if (!b[fd])
+		b[fd] = ft_strnew(0);
+	return (gnl(fd, b, line));
 }
