7b6262e8741b20ae2d85578d35cde2a086f04e9f
lukefreeman42 <lukefreeman@protonmail.com>
2018-12-06 13:58:27 -0800

    .

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include "get_next_line.h"
+
 int main(int argc, char **argv)
 {
 	int fd = open(argv[1], O_RDONLY);
 	char *line;
 	int st;
 	int linenum = 0; 
+	if (argc != 2)
+		return (-1);
 	while ((st = get_next_line(fd, &line)))
 	{
 		linenum += st;
 		printf("%s\n %d\n", line, linenum);
+		free(line);
 	}
-	printf("%s\n %d\n", line, linenum);
 	return (0);
 }
