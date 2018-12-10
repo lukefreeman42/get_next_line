e2f0e209dfa87f6736f059719e9b557db71dfdd1
lukefreeman42 <lukefreeman@protonmail.com>
2018-12-09 22:01:03 -0800

    simple and complete

--- get_next_line.c
+++ get_next_line.c
@@ -5,46 +5,48 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/12/06 09:25:42 by llelias           #+#    #+#             */
-/*   Updated: 2018/12/06 10:13:38 by llelias          ###   ########.fr       */
+/*   Created: 2018/12/09 09:16:06 by llelias           #+#    #+#             */
+/*   Updated: 2018/12/09 14:02:55 by llelias          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		gnl(int fd, char *b[], char **line)
+int		gnl(int fd, char **line, char *f[])
 {
-	int		st;
 	char	rb[BUFF_SIZE + 1];
-	char	*nl_p;
+	char	*nl;
 	char	*tmp;
+	int		r;
 
-	st = -2;
-	while (!(nl_p = ft_strchr(b[fd], '\n')) &&
-			(st = read(fd, rb, BUFF_SIZE)) > 0 &&
-			!(rb[st] = '\0'))
-		b[fd] = ft_fstrjoin(b[fd], rb, 1);
-	tmp = b[fd];
-	if (st == -1)
-		return (-1);
-	if (ft_strlen(b[fd]))
+	nl = NULL;
+	while ((r = read(fd, rb, BUFF_SIZE)) > 0)
 	{
-		if (nl_p && !(*nl_p = '\0'))
-			b[fd] = ft_strdup(++nl_p);
-		*line = ft_fstrdup(tmp);
-		if (ft_strlen(b[fd]) || st)
-			return (1);
+		rb[r] = '\0';
+		f[fd] = ft_fstrjoin(f[fd], rb, 1);
+		if ((nl = ft_strchr(f[fd], '\n')))
+			break ;
 	}
-	return (0);
+	if (r == -1)
+		return (-1);
+	tmp = f[fd];
+	f[fd] = NULL;
+	if (nl)
+		if (!(*nl = '\0'))
+			f[fd] = ft_strdup(++nl);
+	*line = ft_fstrdup(tmp);
+	if (r == 0 && **line == 0)
+		return (0);
+	return (1);
 }
 
 int		get_next_line(int fd, char **line)
 {
-	static char *b[MAX_FD];
+	static char	*f[MAX_FD];
 
-	if (!VALID_FD(fd))
+	if (!VALID(fd))
 		return (-1);
-	if (!b[fd])
-		b[fd] = ft_strnew(0);
-	return (gnl(fd, b, line));
+	if (!f[fd])
+		f[fd] = ft_strnew(0);
+	return (gnl(fd, line, f));
 }
--- main.c
+++ main.c
@@ -1,19 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/09 09:00:39 by llelias           #+#    #+#             */
+/*   Updated: 2018/12/09 13:41:54 by llelias          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <fcntl.h>
 #include <stdio.h>
 #include "get_next_line.h"
+#include <libft.h>
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
-	int fd = open(argv[1], O_RDONLY);
-	char *line;
-	int st;
-	int linenum = 0; 
-	if (argc != 2)
+	int		fd[argc];
+	int		i;
+	char	*line;
+
+	i = argc - 1;
+	if (argc < 2)
 		return (-1);
-	while (1 || (st = get_next_line(fd, &line)))
+	while (i > 0)
 	{
-		linenum += st;
-		printf("%s\n %d\n", line, linenum);
+		fd[i] = open(argv[i], O_RDONLY);
+		i--;
+	}
+	while (i++ + 1 < argc)
+	{
+		while (get_next_line(fd[i], &line) > 0)
+		{
+			printf("%s\n", line);
+			free(line);
+			line = NULL;
+		}
 		free(line);
 	}
 	return (0);
