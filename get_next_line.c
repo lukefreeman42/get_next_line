6ab7c2337a22ab6ce961f079b8e49c0e6727ec8d
lukefreeman42 <lukefreeman@protonmail.com>
2018-12-10 11:02:44 -0800

    fixed for any buffer size

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/09 09:16:06 by llelias           #+#    #+#             */
-/*   Updated: 2018/12/09 14:02:55 by llelias          ###   ########.fr       */
+/*   Updated: 2018/12/09 23:01:47 by llelias          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,22 @@ int		gnl(int fd, char **line, char *f[])
 	char	*tmp;
 	int		r;
 
-	nl = NULL;
 	while ((r = read(fd, rb, BUFF_SIZE)) > 0)
 	{
 		rb[r] = '\0';
 		f[fd] = ft_fstrjoin(f[fd], rb, 1);
-		if ((nl = ft_strchr(f[fd], '\n')))
+		if (ft_strchr(f[fd], '\n'))
 			break ;
 	}
 	if (r == -1)
 		return (-1);
 	tmp = f[fd];
 	f[fd] = NULL;
-	if (nl)
+	if ((nl = ft_strchr(tmp, '\n')))
 		if (!(*nl = '\0'))
 			f[fd] = ft_strdup(++nl);
 	*line = ft_fstrdup(tmp);
-	if (r == 0 && **line == 0)
+	if (r == 0 && **line == 0 && !f[fd])
 		return (0);
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 21:29:49 by llelias           #+#    #+#             */
-/*   Updated: 2018/12/06 14:20:29 by llelias          ###   ########.fr       */
+/*   Updated: 2018/12/10 10:51:14 by llelias          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
-# define MAX_FD 256
-# define VALID_FD(x) (x >= 0 && x < MAX_FD)
-# include <libft.h>
+# define BUFF_SIZE 40
+# define MAX_FD 1024
+# define VALID(x) (x >= 0 && x < MAX_FD)
+# include "libft/libft.h"
 
 int	get_next_line(const int fd, char **line);
 #endif
